82e48e8abe93dfbaa29abde4c374fa63374b2004
NateKulton <natekulton@gmail.com>
2024-07-05 14:55:15 -0400

    testing and notes added

--- lesson_2/calculatorNotes.md
+++ lesson_2/calculatorNotes.md
@@ -1,3 +1,7 @@
 I wasn't sure if LS wanted me take make the language setting in a way where the user could just input something
 to say waht language to use or just let them edit the file to change the language. I think it seemed like they were
 saying the latter thing
+
+Loan calculator:
+
+I need to look into ways to deal with someone inputting years or months into my calculator
--- lesson_2/loanCalculator.js
+++ lesson_2/loanCalculator.js
@@ -1,10 +1,12 @@
-// loanA is loan amount, rate is monthly interest rate, and
-// loanD is loan durartion in months
-// I wanted to names these better but eslint wanted them to too shorter names
+// loanA, is loan amount
+// rate, is monthly interest rate
+// loanD, is loan durartion in months
+// I wanted to name these better but eslint wanted them to  shorter names
+
 
 function loanCalc(loanA, rate, loanD) {
   let pay = loanA * ((rate / 12) / (1 - Math.pow((1 + (rate / 12)), (-loanD))));
   return pay;
 }
 
-console.log(loanCalc(10000, 0.06, 12));
+console.log(loanCalc(10000, 0.10, 2));
