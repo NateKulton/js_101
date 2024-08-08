ff0a0732549c154d8558fdfd3487c540d5c3879c
NateKulton <natekulton@gmail.com>
2024-08-08 16:52:10 -0400

    RPS Bonus Features

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,79 +1,104 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 let anotherRound;
+let userWins = 0;
+let computerWins = 0;
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
-function reformattedUserInput() {
+function userInput() {
   return readline.question().trim().toLowerCase();
 }
 
-function displayWinner(userChoice, computerChoice) {
+function shortHand(userChoice) {
+  if (userChoice === 'r') {
+    userChoice = 'rock';
+    return userChoice;
+  } else if (userChoice === 'p') {
+    userChoice = 'paper';
+    return userChoice;
+  } else if (userChoice === 'sc') {
+    userChoice = 'scissors';
+    return userChoice;
+  } else if (userChoice === 'l') {
+    userChoice = 'lizard';
+    return userChoice;
+  } else if (userChoice === 'sp') {
+    userChoice = 'spock';
+    return userChoice;
+  } else {
+    return userChoice;
+  }
+}
+
+
+function userWinCondition(userChoice, computerChoice) {
   prompt(`You chose ${userChoice}. Computer chose ${computerChoice}.`);
 
-  if ((userChoice === 'rock' && computerChoice === 'scissors') ||
-      (userChoice === 'rock' && computerChoice === 'lizard') ||
-      (userChoice === 'paper' && computerChoice === 'rock') ||
-      (userChoice === 'paper' && computerChoice === 'spock') ||
-      (userChoice === 'scissors' && computerChoice === 'paper') ||
-      (userChoice === 'scissors' && computerChoice === 'lizard') ||
-      (userChoice === 'lizard' && computerChoice === 'paper') ||
-      (userChoice === 'lizard' && computerChoice === 'spock') ||
-      (userChoice === 'spock' && computerChoice === 'scissors') ||
-      (userChoice === 'spock' && computerChoice === 'rock')) {
-    prompt("You won!");
-  } else if ((userChoice === 'rock' && userChoice === 'scissors') ||
-      (computerChoice === 'rock' && userChoice === 'lizard') ||
-      (computerChoice === 'paper' && userChoice === 'rock') ||
-      (computerChoice === 'paper' && userChoice === 'spock') ||
-      (computerChoice === 'scissors' && userChoice === 'paper') ||
-      (computerChoice === 'scissors' && userChoice === 'lizard') ||
-      (computerChoice === 'lizard' && userChoice === 'paper') ||
-      (computerChoice === 'lizard' && userChoice === 'spock') ||
-      (computerChoice === 'spock' && userChoice === 'scissors') ||
-      (computerChoice === 'spock' && userChoice === 'rock')) {
-    prompt("The computer won!");
+  return (userChoice === 'rock' && computerChoice === 'scissors') ||
+         (userChoice === 'rock' && computerChoice === 'lizard') ||
+         (userChoice === 'paper' && computerChoice === 'rock') ||
+         (userChoice === 'paper' && computerChoice === 'spock') ||
+         (userChoice === 'scissors' && computerChoice === 'paper') ||
+         (userChoice === 'scissors' && computerChoice === 'lizard') ||
+         (userChoice === 'lizard' && computerChoice === 'paper') ||
+         (userChoice === 'lizard' && computerChoice === 'spock') ||
+         (userChoice === 'spock' && computerChoice === 'scissors') ||
+         (userChoice === 'spock' && computerChoice === 'rock');
+}
+
+function displayWinner(userChoice, computerChoice) {
+  if (userWinCondition(userChoice, computerChoice)) {
+    return "You won!";
+  } else if (userChoice === computerChoice) {
+    return "It looks like a tie!";
   } else {
-    prompt("It looks like a tie!");
+    return "The computer won!";
   }
 }
 
 do {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
   prompt("Note: shorthand is 'r', 'p', 'sc', 'l', and 'sp' respectively");
-  let userChoice = reformattedUserInput();
-
-  if (userChoice === 'r') {
-    userChoice = 'rock';
-  } else if (userChoice === 'p') {
-    userChoice = 'paper';
-  } else if (userChoice === 'sc') {
-    userChoice = 'scissors';
-  } else if (userChoice === 'l') {
-    userChoice = 'lizard';
-  } else if (userChoice === 'sp') {
-    userChoice = 'spock';
-  }
+  let userChoice = userInput();
+  userChoice = shortHand(userChoice);
 
   while (!VALID_CHOICES.includes(userChoice)) {
+    console.clear();
     prompt(`That's not a valid choice. Valid choices are: ${VALID_CHOICES.join(', ')}`);
 
-    userChoice = reformattedUserInput();
+    userChoice = userInput(shortHand);
+    userChoice = shortHand(userChoice);
   }
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  displayWinner(userChoice, computerChoice);
+  let stringWinner = displayWinner(userChoice, computerChoice);
+  prompt(stringWinner);
+
+  if (stringWinner === "You won!") {
+    userWins += 1;
+  } else if (stringWinner ===  "The computer won!") {
+    computerWins += 1;
+  }
+
+  if (userWins === 3) {
+    prompt('YOU ARE THE GRAND CHAMPION!');
+    break;
+  } else if (computerWins === 3) {
+    prompt('THE COMPUTER IS THE GRAND CHAMPION!');
+    break;
+  }
 
   prompt("Enter 'y' to play again or 'n' to exit");
-  anotherRound = reformattedUserInput();
+  anotherRound = userInput();
 
   while (anotherRound !== 'y' && anotherRound !== 'n') {
     prompt("You have to enter 'y' or 'n'!");
-    anotherRound = reformattedUserInput();
+    anotherRound = userInput();
   }
 
   console.clear();
