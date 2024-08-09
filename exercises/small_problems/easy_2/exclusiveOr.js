3634613daf7b8caf66c0415d7ffe47ea531ef604
NateKulton <natekulton@gmail.com>
2024-08-09 11:22:32 -0400

    SP exclusiveOr.js

---
+++ exercises/small_problems/easy_2/exclusiveOr.js
@@ -0,0 +1,10 @@
+function xor(value1, value2) {
+  return !(value1) !== !(value2);
+}
+
+console.log(xor(5, 0) === true);          // true
+console.log(xor(false, true) === true);   // true
+console.log(xor(1, 1) === false);         // true
+console.log(xor(true, true) === false);   // true
+console.log(xor(0, false) === false);     // true
+console.log(xor(0, 0) === false);         // true
